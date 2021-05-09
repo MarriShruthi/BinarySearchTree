c09dfc99887c2136fa9938f595d7b08bfe745e0e
Shruthi <shruthimarri97@gmail.com>
2021-05-10 04:27:37 +0530

    [Shruthi]:created a Binary search tree

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Binary Search Tree");
+            Console.WriteLine();
+            BST<int> bST = new BST<int>(56);
+            bST.Insert(30);
+            bST.Insert(70);
+            bST.Display();
+
+            Console.Read();
+        }
+    }
+}
